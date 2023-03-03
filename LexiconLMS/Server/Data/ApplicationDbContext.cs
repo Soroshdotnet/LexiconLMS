429391a10ba6c905f0a5799c9a7092d3748f8f46
Attila Starkenius <symphtom@hotmail.com>
2023-03-03 15:34:03 +0100

    Attila Starkenius 3.3.2023. Beginning to make model classes in LexiconLMS.Shared project, to be able to seed Users, Courses, Modules etc.

--- LexiconLMS/Server/Data/ApplicationDbContext.cs
+++ LexiconLMS/Server/Data/ApplicationDbContext.cs
@@ -8,7 +8,24 @@ namespace LexiconLMS.Server.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(
+//        public DbSet<GymClass> GymClasses => Set<GymClass>();
+//        public DbSet<ApplicationUserGymClass> AppUserGymClass => Set<ApplicationUserGymClass>();
+//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        protected override void OnModelCreating(ModelBuilder builder)
+//        {
+//            base.OnModelCreating(builder);
+
+//            builder.Entity<ApplicationUserGymClass>().HasKey(a => new { a.ApplicationUserId, a.GymClassId });
+
+//            builder.Entity<GymClass>().HasQueryFilter(g => g.StartTime > DateTime.UtcNow);
+//        }
+//    }
+//}
+public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
