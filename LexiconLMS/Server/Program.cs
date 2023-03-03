3c339cb2263cbb325f760d38c3601847ab4a95b2
soroshdotnet <sorosh.farahmand@gmail.com>
2023-03-03 15:29:19 +0100

    SignIn.RequireConfirmed = false

--- LexiconLMS/Server/Program.cs
+++ LexiconLMS/Server/Program.cs
@@ -18,7 +18,7 @@ namespace LexiconLMS
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             builder.Services.AddIdentityServer()
